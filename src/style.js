b2de95ad5f935ef8c0f9e376d7b12b09ba1a3a27
Sangwoo Shin <ssangwoo96@gmail.com>
2024-10-24 00:25:55 +0900

    install 오류 해결

---
+++ src/style.js
@@ -0,0 +1,27 @@
+import { css } from "styled-components";
+
+export const flexMixin = (
+  direction = "row",
+  justify = "start",
+  align = "stretch",
+  gap = 0,
+  wrap = "nowrap"
+) => {
+  return `
+    display: flex;
+    flex-direction: ${direction};
+    justify-content: ${justify};
+    align-items: ${align};
+    gap: ${gap};
+    flex-wrap: ${wrap};
+    `;
+};
+
+export const font_size_weight = (size = "size", weight = "weight") => {
+  return `font-size: ${size};
+    font-weight: ${weight};
+`;
+};
+
+export const tag_color = "#d7fa00";
+export const gray = rgb(160, 160, 160);
